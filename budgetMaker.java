d79b50d780d8a224802bf3297fa7fcce23f475e1
Blake Capella <bellis8@comcast.net>
2018-02-24 16:09:46 -0500

    comments

--- budgetMaker.java
+++ budgetMaker.java
@@ -5,18 +5,22 @@ import java.io.IOException;
 
 public class budgetMaker{
     public static Scanner scnr = new Scanner(System.in);
-    public static void setup(){
+   
+   public static void setup(){ // this method runs the first time the program is launched
 	double income, fixedExpenses, budget = 0.0;
-
+	
+	// user inputs information
 	System.out.print("Enter your income: $");
 	income = scnr.nextDouble();
 	System.out.print("Enter your fixed expenses: $");
 	fixedExpenses = scnr.nextDouble();
 
+	//total free money determined
 	budget = income - fixedExpenses;
 	System.out.println("Your budget is $" + budget);
-	String budgetStr = Double.toString(budget);
+	String budgetStr = Double.toString(budget); // numeric value converted to a string
     
+	//to .txt method
 	try {
 			File file = new File("budget.txt");
 			FileWriter fileWriter = new FileWriter(file);
